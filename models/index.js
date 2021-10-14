efc7db5b5e17bd38818adb87c38c4fc7cc88481d
DaveToth77 <85111342+DaveToth77@users.noreply.github.com>
2021-10-14 08:56:57 -0400

    added model associations

--- models/index.js
+++ models/index.js
@@ -6,22 +6,22 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: ''
+  foreignKey: 'category_id'
 })
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: ''
+  foreignKey: 'product_id'
 })
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: ,
-  foreignKey: '',
+  as: product_tag_id,
+  foreignKey: 'tag_id',
 
 })
 // Tags belongToMany Products 
 Tag.belongsToMany(Product, {
-  foreignKey: ''
+  foreignKey: 'product_id'
 })
 module.exports = {
   Product,
